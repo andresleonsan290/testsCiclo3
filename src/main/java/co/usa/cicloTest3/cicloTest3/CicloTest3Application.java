d0101dfbd06155d5ad305f6c51ce59e65b1039f3
Anfelesan <andresleonsan290@gmail.com>
2022-10-11 22:41:03 -0500

    Primer Commit

---
+++ src/main/java/co/usa/cicloTest3/cicloTest3/CicloTest3Application.java
@@ -0,0 +1,14 @@
+package co.usa.cicloTest3.cicloTest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicloTest3Application {
+
+	public static void main(String[] args) {
+                System.out.println("Hola mundo cruel :c");
+		SpringApplication.run(CicloTest3Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/usa/cicloTest3/cicloTest3/CicloTest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.usa.cicloTest3.cicloTest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicloTest3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
